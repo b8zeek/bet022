f83f8dd8900b5cc1cb87ab077c4ef62f58a44336
bejzik8 <bejzik8@gmail.com>
2022-11-20 23:04:37 +0100

    Created Input component.

---
+++ src/components/Input.tsx
@@ -0,0 +1,17 @@
+import styled from 'styled-components'
+
+export const Input = styled.input`
+  min-width: 350px;
+  height: 28px;
+  padding: 0 10px;
+  font-size: 14px;
+  text-align: center;
+  background-color: #0d1117;
+  border: 1px solid #30363d;
+  border-radius: 6px;
+  margin: 30px 0 10px;
+  &:focus {
+    background-color: #161b22;
+    outline: none;
+  }
+`
