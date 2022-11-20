40564e55db3d736020b126dc7aa45093e99e6237
bejzik8 <bejzik8@gmail.com>
2022-11-20 20:39:34 +0100

    Updated base styling.

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import styled from 'styled-components'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+    <Container>
+    </Container>
   )
 }
+
+const Container = styled.div`
+  max-width: 500px;
+  width: 100%;
+  min-height: 300vh;
+  position: relative;
+  background-color: aquamarine;
+  margin: 0 auto;
+`
+
+const Header = styled.header`
+  height: 40px;
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  z-index: 1;
+  background-color: aliceblue;
+`
 
 export default App
