30734478f1b467768b55e04fbdefacc99ac3d345
bejzik8 <bejzik8@gmail.com>
2022-11-21 00:04:09 +0100

    Updated Footer component.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,13 @@
 import styled from 'styled-components'
 
+import { Logo } from './Logo'
+
 export function Footer() {
   return (
     <Container>
-      <Content>123</Content>
+      <Content>
+        <Logo />
+      </Content>
     </Container>
   )
 }
@@ -14,7 +18,6 @@ const Container = styled.footer`
   bottom: 0;
   left: 0;
   right: 0;
-  padding: 12px 0;
   background-color: rgba(255, 255, 255, 0.05);
   backdrop-filter: blur(5px);
   margin: 0;
@@ -23,6 +26,9 @@ const Container = styled.footer`
 const Content = styled.div`
   max-width: 500px;
   width: 100%;
-  height: 40px;
+  height: 64px;
+  display: flex;
+  justify-content: flex-end;
+  padding: 12px 10px;
   margin: 0 auto;
 `
