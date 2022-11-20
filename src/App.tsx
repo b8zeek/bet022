7713a322d7e27ba916a96698da8348ce55124d9f
bejzik8 <bejzik8@gmail.com>
2022-11-21 00:24:00 +0100

    Updated App component.

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import styled from 'styled-components'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
 import { LoginPage, StandingsPage } from './pages'
-import { Footer } from './components'
+import { Navigation } from './components'
 
 const router = createBrowserRouter([
   {
@@ -23,7 +23,10 @@ function App() {
   return (
     <Container>
       <RouterProvider router={router} />
-      <Footer />
+      <Footer>
+        <CopyrightText>Copyright © 2022 🌾 Šid Caffe ☕, Serbia. All Rights Reserved.</CopyrightText>
+      </Footer>
+      <Navigation />
     </Container>
   )
 }
@@ -31,10 +34,22 @@ function App() {
 const Container = styled.div`
   max-width: 500px;
   width: 100%;
-  min-height: 400vh;
+  min-height: 100vh;
   position: relative;
-  padding: 20px 10px;
+  padding: 20px 10px 300px;
   margin: 0 auto;
+`
+
+const Footer = styled.footer`
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  padding: 0 10px 80px;
+`
+
+const CopyrightText = styled.p`
+  font-size: 12px;
 `
 
 export default App
