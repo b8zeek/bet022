bfd6b1a3129378b6a74872c1c3a9610ccb56b6fe
bejzik8 <bejzik8@gmail.com>
2022-11-21 01:27:30 +0100

    Updated LoginPage component.

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,31 +1,19 @@
 import styled from 'styled-components'
 
+import { PageLayout } from '../wrappers'
 import { Heading, Subheading, Input } from '../components'
 
 import { FOOTER_HEIGHT } from '../constants'
 
 export function LoginPage() {
   return (
-    <Container>
-      <Heading rainbow textCenter>
-        Welcome Traveler
-      </Heading>
-      <Subheading marginBottom='20px'>Thank you for your money poor lost soul</Subheading>
+    <PageLayout heading='Welcome Traveler' subheading='Thank you for your money poor lost soul'>
       <Form>
         <Input spellCheck={false} />
       </Form>
-    </Container>
+    </PageLayout>
   )
 }
-
-const Container = styled.div`
-  min-height: calc(100vh - ${FOOTER_HEIGHT});
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  padding: 20px 10px 0;
-`
 
 const Form = styled.form`
   width: 100%;
