9a6d830499e2a112a0ed0369a9823e3afc2353ad
bejzik8 <bejzik8@gmail.com>
2022-11-21 02:36:17 +0100

    Updated useParticipants hook.

--- src/hooks/useParticipants.ts
+++ src/hooks/useParticipants.ts
@@ -2,7 +2,12 @@ import { useQuery } from 'react-query'
 import axios from 'axios'
 
 export function useParticipants() {
-  const getParticipants = () => axios.get('http://localhost:3000/participants').then(res => res.data)
+  const getParticipants = async () => {
+    // TODO: REMOVE NEXT LINE LATER
+    await new Promise(_ => setTimeout(_, 5000))
+
+    return axios.get('http://localhost:3000/participants').then(res => res.data)
+  }
 
   return useQuery(['participants'], getParticipants)
 }
