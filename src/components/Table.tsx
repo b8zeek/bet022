54b70c736b59dc5ce00211a7cfbd58fdb16a93e2
bejzik8 <bejzik8@gmail.com>
2022-11-21 01:48:51 +0100

    Updated Table component.

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,42 +1,72 @@
 import styled from 'styled-components'
 
+import { Participant } from '../models'
+
+type ItemProps = {
+  participant: Participant
+}
+
+function Item({ participant }: ItemProps) {
+  const { name, score } = participant
+
+  return (
+    <Row>
+      {name} - {score}
+    </Row>
+  )
+}
+
+const participants = [
+  {
+    name: 'Goran Panjkov',
+    score: 45
+  },
+  {
+    name: 'Petar Vidovic',
+    score: 44
+  },
+  {
+    name: 'Mirko Basic',
+    score: 32
+  },
+  {
+    name: 'Milos Micic',
+    score: 27
+  },
+  {
+    name: 'Zoran Vlaisavljevic',
+    score: 24
+  },
+  {
+    name: 'Marko Iskic',
+    score: 23
+  },
+  {
+    name: 'Dusan Maukovic',
+    score: 12
+  },
+  {
+    name: 'Bojan Babic',
+    score: 12
+  },
+  {
+    name: 'Ljubo Vidovic',
+    score: 12
+  },
+  {
+    name: 'Kemal El Shairy',
+    score: 0
+  }
+]
+
 export function Table() {
   return (
     <Container>
       <Header>Current Standings</Header>
       <Body>
-        <Row>Mirko</Row>
-        <Row>Basic</Row>
-        <Row>Petar</Row>
-        <Row>Vidovic</Row>
-        <Row>Ljubo</Row>
-        <Row>Vidovic</Row>
-        <Row>Bojan</Row>
-        <Row>Babic</Row>
-        <Row>Mirko</Row>
-        <Row>Basic</Row>
-        <Row>Petar</Row>
-        <Row>Vidovic</Row>
-        <Row>Ljubo</Row>
-        <Row>Vidovic</Row>
-        <Row>Bojan</Row>
-        <Row>Babic</Row>
-        <Row>Mirko</Row>
-        <Row>Basic</Row>
-        <Row>Petar</Row>
-        <Row>Vidovic</Row>
-        <Row>Ljubo</Row>
-        <Row>Vidovic</Row>
-        <Row>Bojan</Row>
-        <Row>Babic</Row>
-        <Row>Mirko</Row>
-        <Row>Basic</Row>
-        <Row>Petar</Row>
-        <Row>Vidovic</Row>
-        <Row>Ljubo</Row>
-        <Row>Vidovic</Row>
-        <Row>Bojan</Row>
-        <Row>Babic</Row>
+        {participants.map(participant => (
+          <Item key={participant.name} participant={participant} />
+        ))}
       </Body>
     </Container>
   )
