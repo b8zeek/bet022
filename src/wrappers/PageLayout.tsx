7f7c80c008fcd1f8a5cdcae4d495372bed0134b0
bejzik8 <bejzik8@gmail.com>
2022-11-21 01:27:20 +0100

    Created PageLayout component.

--- src/wrappers/PageLayout.tsx
+++ src/wrappers/PageLayout.tsx
@@ -1,5 +1,33 @@
+import { ReactNode } from 'react'
 import styled from 'styled-components'
 
-export function PageLayout() {}
+import { Heading, Subheading } from '../components'
 
-const Container = styled.div``
+import { FOOTER_HEIGHT } from '../constants'
+
+type PageLayoutProps = {
+  heading: string
+  subheading: string
+  children: ReactNode | ReactNode[]
+}
+
+export function PageLayout({ heading, subheading, children }: PageLayoutProps) {
+  return (
+    <Container>
+      <Heading rainbow textCenter>
+        {heading}
+      </Heading>
+      <Subheading marginBottom='20px'>{subheading}</Subheading>
+      {children}
+    </Container>
+  )
+}
+
+const Container = styled.div`
+  min-height: calc(100vh - ${FOOTER_HEIGHT});
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 20px 10px 0;
+`
