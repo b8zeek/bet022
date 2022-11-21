c589a6b4427a0d4b144d651a9edfcb438e45a43f
bejzik8 <bejzik8@gmail.com>
2022-11-21 01:08:00 +0100

    Created Subheading component.

---
+++ src/components/Subheading.tsx
@@ -0,0 +1,15 @@
+import styled from 'styled-components'
+
+type SubheadingProps = {
+  marginBottom?: string
+}
+
+export const Subheading = styled.h2<SubheadingProps>`
+  line-height: 24px;
+  font-size: 16px;
+  font-weight: 300;
+  font-style: italic;
+  color: #ffffffb0;
+
+  ${({ marginBottom }) => marginBottom && `margin-bottom: ${marginBottom};`}
+`
