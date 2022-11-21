00b6304d5f9c8926a6506d4cace8757729b7e62d
bejzik8 <bejzik8@gmail.com>
2022-11-21 02:36:34 +0100

    Updated StandingsPage component.

--- src/components/Subheading.tsx
+++ src/components/Subheading.tsx
@@ -9,6 +9,7 @@ export const Subheading = styled.h2<SubheadingProps>`
   font-size: 16px;
   font-weight: 300;
   font-style: italic;
+  text-align: center;
   color: #ffffffb0;
 
   ${({ marginBottom }) => marginBottom && `margin-bottom: ${marginBottom};`}
--- src/pages/StandingsPage.tsx
+++ src/pages/StandingsPage.tsx
@@ -3,10 +3,14 @@ import styled from 'styled-components'
 import { PageLayout } from '../wrappers'
 import { Table } from '../components'
 
+import { useParticipants } from '../hooks'
+
 export function StandingsPage() {
+  const { data: participants, isLoading, isError } = useParticipants()
+
   return (
     <PageLayout heading='Current Standings' subheading='You have to try much harder buddy!'>
-      <Table />
+      {!isLoading && <Table participants={participants} />}
     </PageLayout>
   )
 }
