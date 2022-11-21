5bb04c21fd30c93a48352c7c8e672b35ef2a9ab7
bejzik8 <bejzik8@gmail.com>
2022-11-21 20:03:08 +0100

    Updated BettingPage and mock data.

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,1 +1,2 @@
 export * from './Participant'
+export * from './Game'
--- src/pages/BettingPage.tsx
+++ src/pages/BettingPage.tsx
@@ -2,10 +2,22 @@ import styled from 'styled-components'
 
 import { PageLayout } from '../wrappers'
 
+import { Game } from '../models'
+
+type BettingProps = {
+  games: Game[]
+}
+
 export function BettingPage() {
   return (
     <PageLayout heading='Your Predictions' subheading='Hmmm... Are you sure about that?'>
-      123
+      <Form>
+        <GameContainer>Game 1</GameContainer>
+      </Form>
     </PageLayout>
   )
 }
+
+const Form = styled.form``
+
+const GameContainer = styled.div``
