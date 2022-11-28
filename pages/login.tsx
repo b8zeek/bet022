ab654711d62c6506e0b2168152417b3ec3c270b2
bejzik8 <bejzik8@gmail.com>
2022-11-28 15:18:01 +0100

    Working on Login page.

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,14 +1,38 @@
 import styled from 'styled-components'
+import { useForm } from 'react-hook-form'
+import { object, string } from 'yup'
+import { yupResolver } from '@hookform/resolvers/yup'
 
 import { PageLayout } from '../wrappers'
-import { Input, RedirectContainer } from '../components'
+import { Input, RedirectContainer, Button } from '../components'
+
+const schema = object().shape({
+  userName: string().min(6).max(255).required(),
+  password: string().min(6).max(255).required()
+})
 
 export default function LoginPage() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset
+  } = useForm({
+    resolver: yupResolver(schema)
+  })
+
+  const onSubmitHandler = (data: any) => {
+    console.log('DATA', { data })
+    reset()
+  }
+
   return (
     <PageLayout heading='Welcome Back Traveler' subheading='Thank you for your money poor lost soul'>
       <RedirectContainer paragraph="Don't have an accout?" redirectLabel='Register' redirectPath='/register' />
-      <Form>
-        <Input spellCheck={false} />
+      <Form onSubmit={handleSubmit(onSubmitHandler)}>
+        <Input spellCheck={false} {...register('userName')} />
+        <Input spellCheck={false} {...register('password')} />
+        <Button type='submit'>Submit</Button>
       </Form>
     </PageLayout>
   )
