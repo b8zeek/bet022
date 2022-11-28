4fc7df891b1abda59ee73f5836769aa8254845a1
bejzik8 <bejzik8@gmail.com>
2022-11-28 15:27:00 +0100

    Updated Input component.

--- components/Input.tsx
+++ components/Input.tsx
@@ -1,6 +1,8 @@
 import styled from 'styled-components'
 
-export const Input = styled.input`
+export const Input = styled.input<{
+  marginBottom?: string
+}>`
   min-width: 350px;
   height: 28px;
   padding: 0 10px;
@@ -9,6 +11,8 @@ export const Input = styled.input`
   background-color: #0d1117;
   border: 1px solid #30363d;
   border-radius: 6px;
+
+  ${({ marginBottom }) => marginBottom && `margin-bottom: ${marginBottom};`}
 
   &:focus {
     background-color: #161b22;
