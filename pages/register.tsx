b3358392b251cbc8b5b1baf8d42ffa6edb26c4c4
bejzik8 <bejzik8@gmail.com>
2022-11-28 14:41:45 +0100

    Updated Register page.

--- pages/register.tsx
+++ pages/register.tsx
@@ -1,18 +1,12 @@
 import styled from 'styled-components'
 
 import { PageLayout } from '../wrappers'
-import { Input, Paragraph, NavButton } from '../components'
+import { Input, Paragraph, NavButton, RedirectContainer } from '../components'
 
 export default function LoginPage() {
   return (
     <PageLayout heading='Welcome Traveler' subheading='We love people like you!'>
-      <RedirectContainer>
-        <Paragraph textCenter marginRight='20px'>
-          Already have an accout?
-        </Paragraph>
-        <NavButton href='/login'>Login</NavButton>
-      </RedirectContainer>
-
+      <RedirectContainer paragraph='Already have an accout?' redirectLabel='Login' redirectPath='/login' />
       <Form>
         <Input spellCheck={false} />
       </Form>
@@ -26,9 +20,4 @@ const Form = styled.form`
   justify-content: center;
   align-items: center;
   flex-direction: column;
-`
-
-const RedirectContainer = styled.div`
-  text-align: center;
-  margin-bottom: 100px;
 `
