d34e2faf066ce8441d7eae76230243408f861031
bejzik8 <bejzik8@gmail.com>
2022-11-28 15:27:08 +0100

    Updated Login page.

--- pages/login.tsx
+++ pages/login.tsx
@@ -4,7 +4,7 @@ import { object, string } from 'yup'
 import { yupResolver } from '@hookform/resolvers/yup'
 
 import { PageLayout } from '../wrappers'
-import { Input, RedirectContainer, Button } from '../components'
+import { Label, Input, RedirectContainer, Button } from '../components'
 
 const schema = object().shape({
   userName: string().min(6).max(255).required(),
@@ -30,8 +30,10 @@ export default function LoginPage() {
     <PageLayout heading='Welcome Back Traveler' subheading='Thank you for your money poor lost soul'>
       <RedirectContainer paragraph="Don't have an accout?" redirectLabel='Register' redirectPath='/register' />
       <Form onSubmit={handleSubmit(onSubmitHandler)}>
-        <Input spellCheck={false} {...register('userName')} />
-        <Input spellCheck={false} {...register('password')} />
+        <Label>Username</Label>
+        <Input spellCheck={false} {...register('userName')} marginBottom='20px' />
+        <Label>Password</Label>
+        <Input spellCheck={false} {...register('password')} marginBottom='40px' />
         <Button type='submit'>Submit</Button>
       </Form>
     </PageLayout>
