c622607db8a83165560bbe6efd5c3234fb412e46
bejzik8 <bejzik8@gmail.com>
2022-11-28 14:24:23 +0100

    Created NavButton component.

---
+++ components/NavButton.tsx
@@ -0,0 +1,17 @@
+import styled from 'styled-components'
+import Link from 'next/link'
+
+export const NavButton = styled(Link)`
+  height: 36px;
+  line-height: 36px;
+  display: inline-block;
+  vertical-align: middle;
+  font-size: 16px;
+  font-weight: 800;
+  margin-right: 12px;
+  border-radius: 6px;
+  padding: 0 12px;
+  background-color: #00000099;
+  text-decoration: none;
+  cursor: pointer;
+`
