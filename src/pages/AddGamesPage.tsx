7878185bbe48b4b5b708e353eef65cc380c7c12b
bejzik8 <bejzik8@gmail.com>
2022-11-30 00:21:57 +0100

    Created GameItem component.

--- src/pages/AddGamesPage.tsx
+++ src/pages/AddGamesPage.tsx
@@ -1,10 +1,49 @@
+import styled from 'styled-components'
 import { PageLayout } from '../wrappers'
-import { Standings, Paragraph } from '../components'
+import { useForm } from 'react-hook-form'
+import { object, string } from 'yup'
+import { yupResolver } from '@hookform/resolvers/yup'
+
+import { Label, Input } from '../components'
+
+function GameItem() {
+  return (
+    <ItemContainer>
+      <Label>Teams</Label>
+      <Teams>
+        <Input marginBottom='5px' />
+        <Input />
+      </Teams>
+    </ItemContainer>
+  )
+}
 
 export function AddGamesPage() {
   return (
     <PageLayout heading='Add Games' subheading='Hmmm...'>
-      <div>Add games!</div>
+      <Form>
+        <GameItem />
+        <GameItem />
+        <GameItem />
+        <GameItem />
+      </Form>
     </PageLayout>
   )
 }
+
+const Form = styled.form`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`
+
+const ItemContainer = styled.div`
+  width: 100%;
+  margin-bottom: 10px;
+`
+
+const Teams = styled.div`
+  width: 100%;
+`
