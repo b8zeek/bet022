075e294b66bba8fbdadba0e84ce150b247b4adce
bejzik8 <bejzik8@gmail.com>
2022-11-29 05:22:10 +0100

    Created AddGamesPage.

---
+++ src/pages/AddGamesPage.tsx
@@ -0,0 +1,10 @@
+import { PageLayout } from '../wrappers'
+import { Standings, Paragraph } from '../components'
+
+export function AddGamesPage() {
+  return (
+    <PageLayout heading='Add Games' subheading='Hmmm...'>
+      <div>Add games!</div>
+    </PageLayout>
+  )
+}
