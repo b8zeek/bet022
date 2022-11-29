c49d62bdeee0cf03435123e97b0c08fb34b7a5da
bejzik8 <bejzik8@gmail.com>
2022-11-29 04:41:25 +0100

    Updated fetching hooks.

--- src/hooks/useRegister.ts
+++ src/hooks/useRegister.ts
@@ -1,12 +1,12 @@
 import { useMutation } from 'react-query'
-import axios from 'axios'
+import { Axios } from '../services'
 
 export function useRegister() {
   const registerUser = async (userData: any) => {
     // TODO: REMOVE NEXT LINE LATER
     await new Promise(_ => setTimeout(_, 1000))
 
-    const res = await axios.post(`${import.meta.env.VITE_URL}/register`, userData)
+    const res = await Axios.post('/register', userData)
 
     return res
   }
--- src/services/index.ts
+++ src/services/index.ts
@@ -1,1 +1,2 @@
 export * from './useUserInterface'
+export * from './axios'
