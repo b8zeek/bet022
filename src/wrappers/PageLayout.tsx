4bf9d041721382f84b352e22e4760a21731eb36a
bejzik8 <bejzik8@gmail.com>
2022-11-29 03:54:26 +0100

    Added spinner to PageLayout.

--- src/wrappers/PageLayout.tsx
+++ src/wrappers/PageLayout.tsx
@@ -1,5 +1,9 @@
 import { ReactNode } from 'react'
 import styled from 'styled-components'
+import { RotatingLines } from 'react-loader-spinner'
+
+import { showSpinnerAtom } from '../store'
+import { useAtomValue } from 'jotai'
 
 import { Heading, Subheading, Navigation, Footer } from '../components'
 
@@ -12,8 +16,15 @@ type PageLayoutProps = {
 }
 
 export function PageLayout({ heading, subheading, children }: PageLayoutProps) {
+  const showSpinner = useAtomValue(showSpinnerAtom)
+
   return (
     <Container>
+      {showSpinner && (
+        <Backdrop>
+          <RotatingLines width='150' strokeColor='#ffffff99' strokeWidth='5' />
+        </Backdrop>
+      )}
       <Page>
         <Navigation />
         <Heading rainbow textCenter>
@@ -40,4 +51,18 @@ const Page = styled.div`
   min-height: calc(100vh - ${FOOTER_HEIGHT});
   padding: 84px 10px 10px;
   margin: 0 auto;
+`
+
+const Backdrop = styled.div`
+  width: 100%;
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  z-index: 2;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #000000bb;
 `
