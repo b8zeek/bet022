04a3f1f3b65a195c8f0841622300443e71a06ff9
bejzik8 <bejzik8@gmail.com>
2022-11-30 02:42:11 +0100

    Working on AddGamesPage's form.

--- src/pages/AddGamesPage.tsx
+++ src/pages/AddGamesPage.tsx
@@ -1,34 +1,61 @@
 import styled from 'styled-components'
 import { PageLayout } from '../wrappers'
-import { useForm } from 'react-hook-form'
+import { useForm, useFieldArray } from 'react-hook-form'
 import { object, string } from 'yup'
 import { yupResolver } from '@hookform/resolvers/yup'
 
 import { Label, Input, Button } from '../components'
 import { useState } from 'react'
 
-function GameItem() {
+type GameItemProps = {
+  register: any
+  index: number
+}
+
+function GameItem({ register, index }: GameItemProps) {
   return (
     <ItemContainer>
       <Label>Teams</Label>
       <Teams>
-        <Input marginBottom='5px' />
-        <Input />
+        <Input {...register(`games.${index}.homeTeam`)} marginBottom='5px' />
+        <Input {...register(`games.${index}.awayTeam`)} />
       </Teams>
       <Label>Time</Label>
       <Teams>
-        <Input />
+        <Input {...register(`games.${index}.date`)} />
       </Teams>
     </ItemContainer>
   )
 }
 
 export function AddGamesPage() {
+  const { control, register, handleSubmit } = useForm()
+  const { fields, append } = useFieldArray({
+    control,
+    name: 'games'
+  })
+
+  const addNewGame = () =>
+    append({
+      homeTeam: '',
+      awayTeam: '',
+      date: ''
+    })
+
+  const onSubmitHandler = (data: any) => {
+    console.log('DATA', data)
+  }
+
   return (
     <PageLayout heading='Add Games' subheading='Hmmm...'>
-      <Form>
+      <Form onSubmit={handleSubmit(onSubmitHandler)}>
+        {fields.map((field, index) => (
+          <GameItem key={field.id} register={register} index={index} />
+        ))}
         <Buttons>
-          <Button type='button'>Add New Game</Button>
+          <Button type='button' onClick={addNewGame}>
+            Add New Game
+          </Button>
           <Button type='button'>Add New Special</Button>
         </Buttons>
         <Button type='submit'>Submit</Button>
