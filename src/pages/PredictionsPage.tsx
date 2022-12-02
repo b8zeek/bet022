6093df79aea8a98cf4554b4d38e6ddafd00eb71d
bejzik8 <bejzik8@gmail.com>
2022-12-02 01:43:00 +0100

    Updated PredictionsPage.

--- src/pages/PredictionsPage.tsx
+++ src/pages/PredictionsPage.tsx
@@ -3,11 +3,11 @@ import styled from 'styled-components'
 import { useForm, useFieldArray } from 'react-hook-form'
 
 import { PageLayout } from '../wrappers'
-
-import { Paragraph, GameItem, SpecialItem } from '../components'
+import { Paragraph, GameItem, SpecialItem, Button } from '../components'
 
 import { Event, Game, Special } from '../models'
 
+import { useUserInterface } from '../services'
 import { useEvents } from '../hooks'
 
 export function PredictionsPage() {
@@ -16,15 +16,17 @@ export function PredictionsPage() {
     specials: Special[]
   }>()
 
-  const { fields: games, append: appendGame } = useFieldArray({
+  const { fields: games } = useFieldArray({
     control,
     name: 'games'
   })
 
-  const { fields: specials, append: appendSpecial } = useFieldArray({
+  const { fields: specials } = useFieldArray({
     control,
     name: 'specials'
   })
+
+  const { showSpinner, hideSpinner } = useUserInterface()
 
   const { data: events, isLoading, isError } = useEvents()
 
@@ -38,22 +40,39 @@ export function PredictionsPage() {
     }
   }, [events])
 
+  const onSubmitHandler = async (data: any) => {
+    showSpinner()
+
+    console.log('FORM DATA', data)
+
+    try {
+      // await mutateAsync(parsedData)
+    } catch (error) {
+      console.log(error)
+    }
+
+    hideSpinner()
+  }
+
   return (
     <PageLayout heading='Upcoming Events' subheading='Take your best shot!'>
       {isError && <Paragraph textCenter>Error while fetching the data.</Paragraph>}
       {isLoading && <Paragraph textCenter>Loading...</Paragraph>}
       {!events?.length && <Paragraph textCenter>No data!</Paragraph>}
 
-      <Form>
+      <Form onSubmit={handleSubmit(onSubmitHandler)}>
         {games.map(game => (
           <GameItem key={game._id} game={game} />
         ))}
         {specials.map(special => (
           <SpecialItem key={special._id} special={special} />
         ))}
+        <Button type='submit'>Submit</Button>
       </Form>
     </PageLayout>
   )
 }
 
-const Form = styled.form``
+const Form = styled.form`
+  text-align: center;
+`
