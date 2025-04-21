059ec4b4dc75f7c6ffde6b98a9da4065aace67cd
Mirko Basic <bejzik8@gmail.com>
2025-04-21 13:38:09 +0200

    Updated useStandings hook.

--- src/hooks/useStandings.ts
+++ src/hooks/useStandings.ts
@@ -6,7 +6,7 @@ export function useStandings() {
     // TODO: REMOVE NEXT LINE LATER
     await new Promise(_ => setTimeout(_, 2000))
 
-    const { data } = await axios.get(`${import.meta.env.VITE_URL}/standings`)
+    const { data } = await axios.get(`${import.meta.env.VITE_URL}/user/standings`)
 
     return data?.standings || []
   }
