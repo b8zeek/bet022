eea588d4862c64beb305e8effe6372d20b57480e
Mirko Basic <bejzik8@gmail.com>
2025-04-24 02:38:44 +0200

    Fixed useStandings hook.

--- src/hooks/useStandings.ts
+++ src/hooks/useStandings.ts
@@ -1,12 +1,12 @@
 import { useQuery } from 'react-query'
-import axios from 'axios'
+import { Axios } from '../services'
 
 export function useStandings() {
   const getStandings = async () => {
     // TODO: REMOVE NEXT LINE LATER
     await new Promise(_ => setTimeout(_, 2000))
 
-    const { data } = await axios.get(`${import.meta.env.VITE_URL}/user/standings`)
+    const { data } = await Axios.get(`${import.meta.env.VITE_URL}/user/standings`)
 
     return data?.standings || []
   }
